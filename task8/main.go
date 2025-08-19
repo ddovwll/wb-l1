7572b25fcad5593bf2a0014d25d181411c0708df
Robert <robertinkashenoid@gmail.com>
2025-08-20 01:49:28 +0300

    complete l1.8

---
+++ task8/main.go
@@ -0,0 +1,29 @@
+package main
+
+func swapBits(num int64, bitPos int, bitToSet int) {
+	if bitPos < 1 || bitPos > 64 {
+		println("bitPos out of range")
+		return
+	}
+
+	switch bitToSet {
+	case 0:
+		num = num & ^int64(1<<(bitPos-1))
+		println(num)
+		break
+	case 1:
+		num = num | int64(1<<(bitPos-1))
+		println(num)
+		break
+	default:
+		println("bitToSet out of range")
+	}
+}
+
+func main() {
+	var num int64 = 5
+	bitPos := 1
+	bitToSet := 0
+
+	swapBits(num, bitPos, bitToSet)
+}
