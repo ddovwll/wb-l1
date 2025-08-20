d47620f2cfd1f7df1e3e7bb2a9b7e69cd6d84028
Robert <robertinkashenoid@gmail.com>
2025-08-20 15:51:54 +0300

    fix l1.17

--- task17/main.go
+++ task17/main.go
@@ -7,21 +7,23 @@ import (
 )
 
 func binSearch[T constraints.Ordered](arr []T, target T) int {
-	l := -1
-	r := len(arr)
-	for l < r-1 {
-		m := (l + r) / 2
+	l := 0
+	r := len(arr) - 1
+	for l <= r {
+		m := l + (r-l)/2
 		if arr[m] < target {
-			l = m
+			l = m + 1
+		} else if arr[m] > target {
+			r = m - 1
 		} else {
-			r = m
+			return m
 		}
 	}
 
-	return r
+	return -1
 }
 
 func main() {
 	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(binSearch(arr, 8))
+	fmt.Println(binSearch(arr, 7))
 }
